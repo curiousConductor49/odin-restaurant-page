c8c9337137c37036982f7d6c2e52e4f435da6466
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-20 15:28:14 -0500

    feat: import and call module function for menu page content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { createAndAppendHomeContent } from "./home-page.js";
+import { createAndAppendMenuContent } from "./menu-page.js";
 
-createAndAppendHomeContent(document.querySelector("#content"));
+// createAndAppendHomeContent(document.querySelector("#content"));
+// createAndAppendMenuContent(document.querySelector("#content"));
