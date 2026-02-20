47728da04e57fcfae0d57f522225e9104850216d
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-20 14:45:08 -0500

    chore: add comments to sketch out DOM elements to be created and appended

---
+++ src/menu-page.js
@@ -0,0 +1,13 @@
+function createAndAppendMenuContent(primaryDivContainer) {
+    // PAGE STRUCTURE
+    // Menu section (uls with li items, each li item has the menu item and its price)
+        // Menu heading (h1)
+        // Appetizers section – heading (h2) + ul w/ Cram canapés, Salmon croquettes, Creamy clam dip and chips,Lobster sliders
+        // Soups section – heading (h2) + ul w/ Creamy clam chowder, Oyster soup, Lobster chowder, Crustacean Brew (Prawns, Shrimp, Crawfish)
+        // Salads section – heading (h2) + ul w/ Cucumber and smoked mackerel salad, Spinach and smoked salmon salad, Avacado and king crab salad
+        // Mains section – heading (h2) + ul w/ Sheet pan salmon, Smoked cheddar shrimp, Seared scallops, Steamed mussels, Roasted halibut, Cod cakes, Baked stuffed clams, Lobster rolls
+        // Desserts section – heading (h2) + ul w/ Sorbet with fruit tarts, Parfaits, Key lime pie, Blueberry pie, Tiramisu, Chocolate lava cake, Carrot cake, Cheesecake, Boston cream pie
+    // 
+}
+
+export { createAndAppendMenuContent };
