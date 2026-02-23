6a0383026a937678d9f03c2db94754019d5d18b0
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-23 15:23:23 -0500

    chore: add pseudocode for tab switch logic

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import { createAndAppendAboutContent } from "./about-page.js";
 // createAndAppendHomeContent(document.querySelector("#content"));
 // createAndAppendMenuContent(document.querySelector("#content"));
 // createAndAppendAboutContent(document.querySelector("#content"));
+
+// pseudocode!!
+// store the DOM content div + btn elements in variables
+// add event listeners to the buttons
+// the anon callback function should remove the current child section of the content div (use a switch statement?), then call the button's respective page-loader function
