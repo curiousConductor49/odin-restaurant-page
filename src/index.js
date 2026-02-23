06e5c8f65a2b88b40a0b928a09a20e9c8923c003
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-23 15:16:53 -0500

    feat: import module function to display about page content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { createAndAppendHomeContent } from "./home-page.js";
 import { createAndAppendMenuContent } from "./menu-page.js";
+import { createAndAppendAboutContent } from "./about-page.js";
 
 // createAndAppendHomeContent(document.querySelector("#content"));
 // createAndAppendMenuContent(document.querySelector("#content"));
+// createAndAppendAboutContent(document.querySelector("#content"));
