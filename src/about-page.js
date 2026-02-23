3ea50610477c2dd355a9f2fe8da050a5545dccd6
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-23 13:52:50 -0500

    feat: create container and first section for about page content

--- src/about-page.js
+++ src/about-page.js
@@ -1,5 +1,22 @@
 function createAndAppendAboutContent(primaryDivContainer) {
-    // origin, goal, location
+    // create page content container
+    const aboutContainer = document.createElement("section");
+    const aboutHeading = document.createElement("h1");
+    aboutHeading.textContent = "About Us";
+    aboutContainer.appendChild(aboutHeading);
+    
+    // create story section and inner elements
+    const storySection = document.createElement("section");
+    storySection.setAttribute("id", "story-container");
+
+    const storyHeading = document.createElement("h2");
+    storyHeading.textContent = "Our Story";
+    storySection.appendChild(storyHeading);
+
+    // const goalSection = document.createElement("section");
+    // goalSection.setAttribute("id", "goal-container");
+    // const locationSection = document.createElement("section");
+    // locationSection.setAttribute("id", "location-container");
 }
 
 export { createAndAppendAboutContent };
