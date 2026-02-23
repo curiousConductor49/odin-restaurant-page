7845b2b6a9dd8f71ca29b20c46207147733a3ec1
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-23 15:16:11 -0500

    fix: rename improperly named instance of a variable

--- src/about-page.js
+++ src/about-page.js
@@ -22,17 +22,17 @@ function createAndAppendAboutContent(displayDiv) {
 
     const visionHeading = document.createElement("h2");
     visionHeading.textContent = "Our Vision";
-    visionHeading.appendChild(goalHeading);
+    visionSection.appendChild(visionHeading);
 
     const visionDescription1 = document.createElement("p");
     visionDescription1.textContent = "We at the Grand Blue have always strived to provide our customers with top-class culinary experience without compromising the safety of you or the breath-taking scenery you dine in."
     const visionDescription2 = document.createElement("p");
     visionDescription2.textContent = "We emphasize cutting-edge technology to service our establishment and uphold a safety-first policy with year-round checks and inspections. Regular consulting sessions are held with ocean wildlife experts and habitat specialists to conform all aspects of the Grand Blue, whether it be waste dispoal, electrical consumption, or supply sources, to nature's needs.";
     const visionDescription3 = document.createElement("p");
-    visionDescription3.textContent = "All on-site staff undergo intensive training, including diving lessons and first aid as part of the onboarding process. All customers are required to complete safety and guidance modules as outlined by our establishment upon entry. All ingredients for our menu items are sourcd sustainably and responsibly.";
+    visionDescription3.textContent = "All on-site staff undergo intensive training, including diving lessons and first aid as part of the onboarding process. All customers are required to complete safety and guidance modules as outlined by our establishment upon entry. All ingredients for our menu items are sourced sustainably and responsibly.";
 
     const blockquoteContainer = document.createElement("div");
-    quoteBlockContainer.setAttribute("id", "blockquote-container");
+    blockquoteContainer.setAttribute("id", "blockquote-container");
     const founderQuoteBlock = document.createElement("blockquote");
     const founderQuoteText = document.createElement("p");
     founderQuoteText.textContent = "Never forget the Grand Blue brings you to nature, not the other way around. You're eating in the living room of a blue whale, but you're as small as shrimp on the grand scale of things, so don't bang the cutlery."
@@ -61,7 +61,7 @@ function createAndAppendAboutContent(displayDiv) {
     locationSection.appendChild(locationHeading);
 
     const locationDescription = document.createElement("p");
-    locationDescription.textContent = "The Grand Blue currently has establishments open in eight different locations across the world, all of them situated for optimal viewing and the least disturbance to marine ecosystems and whale migration routes. We hope to open a ninth and tenth shortly within the first half of 20XX. Please contact one of our outreach staff for further details if you wish to book a trip or place a reservation."
+    locationDescription.textContent = "The Grand Blue currently has establishments open in eight different locations across the world, all of them situated for optimal viewing and the least disturbance to marine ecosystems and whale migration routes. We hope to open a ninth and tenth location shortly within the first half of 20XX. Please contact one of our outreach staff for further details if you wish to book a trip or place a reservation."
     locationSection.appendChild(locationDescription);
 
     // append sections to page content container, then to the display div
