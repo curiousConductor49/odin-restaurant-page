94a04e3d61a2551ddee6b8663bf230f4e9a93b96
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-23 16:21:30 -0500

    chore: remove pseudocode and old commented-out code

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import { createAndAppendHomeContent } from "./home-page.js";
 import { createAndAppendMenuContent } from "./menu-page.js";
 import { createAndAppendAboutContent } from "./about-page.js";
 
-// createAndAppendHomeContent(document.querySelector("#content"));
-// createAndAppendMenuContent(document.querySelector("#content"));
-// createAndAppendAboutContent(document.querySelector("#content"));
-
-// pseudocode!!
-// store the DOM content div + btn elements in variables
-// add event listeners to the buttons
-// the anon callback function should remove the current child section of the content div (use a switch statement?), then call the button's respective page-loader function
-// compare the btn being clicked versus whether the div is currently populated
-// if the div is populated, check if it's the same tab that's being clicked, if so then just return, if not then use conditional logic to load the right page based on the btn being being clicked
-// if the div is not populated, then just load the page based on the btn being clicked
-
 const contentDiv = document.querySelector("#content");
 const homeTab = document.querySelector("#home-tab");
 const menuTab = document.querySelector("#menu-tab");
