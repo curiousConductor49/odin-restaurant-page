0405411e0fc6589e81059a61579af31fc1ab0c64
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-02-23 16:20:58 -0500

    feat: add tab switching logic

--- src/index.js
+++ src/index.js
@@ -10,3 +10,48 @@ import { createAndAppendAboutContent } from "./about-page.js";
 // store the DOM content div + btn elements in variables
 // add event listeners to the buttons
 // the anon callback function should remove the current child section of the content div (use a switch statement?), then call the button's respective page-loader function
+// compare the btn being clicked versus whether the div is currently populated
+// if the div is populated, check if it's the same tab that's being clicked, if so then just return, if not then use conditional logic to load the right page based on the btn being being clicked
+// if the div is not populated, then just load the page based on the btn being clicked
+
+const contentDiv = document.querySelector("#content");
+const homeTab = document.querySelector("#home-tab");
+const menuTab = document.querySelector("#menu-tab");
+const aboutTab = document.querySelector("#about-tab");
+
+// default for when page is landed on
+createAndAppendHomeContent(contentDiv);
+
+function switchTab(event) {
+    switch (event.target) {
+        case homeTab:
+            if (contentDiv.contains(homeTab)) {
+                return;
+            } else {
+                contentDiv.replaceChildren();
+                createAndAppendHomeContent(contentDiv);
+            }
+            break;
+        case menuTab:
+            if (contentDiv.contains(menuTab)) {
+                return;
+            } else {
+                contentDiv.replaceChildren();
+                createAndAppendMenuContent(contentDiv);
+            }
+            break;
+        case aboutTab:
+            if (contentDiv.contains(aboutTab)) {
+                return;
+            } else {
+                contentDiv.replaceChildren();
+                createAndAppendAboutContent(contentDiv);
+            }
+            break;
+    }
+}
+
+// tab switching logic
+homeTab.addEventListener("click", switchTab);
+menuTab.addEventListener("click", switchTab);
+aboutTab.addEventListener("click", switchTab);
